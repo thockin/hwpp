13fb245b684832c0a7ea4128fedf915ed63bbf9b
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2007-05-30 00:46:02 +0000

    Add pp_device based on pp_scope.  Remove pp_dirent.

---
+++ trunk/tests/pp_device_test.cpp
@@ -0,0 +1,136 @@
+#include "pp_device.h"
+#include "pp.h"
+#include "pp_test.h"
+#include "pp_test_binding.h"
+#include "pp_datatypes.h"
+#include "pp_fields.h"
+#include "keyed_vector.h"
+using namespace std;
+
+void
+dump_scope(const pp_scope_ptr &scope)
+{
+	for (size_t i = 0; i < scope->constants.size(); i++) {
+		cout << "constant: ";
+		cout << scope->constants.key_at(i) << " = "
+		     << scope->constants[i] << endl;
+	}
+	for (size_t i = 0; i < scope->datatypes.size(); i++) {
+		cout << "datatype: "
+		     << scope->datatypes.key_at(i) << endl;
+	}
+	for (size_t i = 0; i < scope->registers.size(); i++) {
+		cout << "register: "
+		     << scope->registers.key_at(i) << endl;
+	}
+	for (size_t i = 0; i < scope->dirents.size(); i++) {
+		cout << "dirent:   "
+		     << scope->dirents[i].type() << ": "
+		     << scope->dirents.key_at(i) << endl;
+		if (scope->dirents[i].is_scope()) {
+			pp_scope_ptr sub = scope->dirents[i].scope();
+			dump_scope(sub);
+		}
+	}
+}
+
+void
+dump_device(const pp_device_ptr &dev)
+{
+	for (size_t i = 0; i < dev->constants.size(); i++) {
+		cout << "constant: ";
+		cout << dev->constants.key_at(i) << " = "
+		     << dev->constants[i] << endl;
+	}
+	for (size_t i = 0; i < dev->datatypes.size(); i++) {
+		cout << "datatype: "
+		     << dev->datatypes.key_at(i) << endl;
+	}
+	for (size_t i = 0; i < dev->dirents.size(); i++) {
+		cout << "dirent:   "
+		     << dev->dirents[i].type() << ": "
+		     << dev->dirents.key_at(i) << endl;
+		if (dev->dirents[i].is_space()) {
+			pp_space_ptr sub = dev->dirents[i].space();
+			dump_scope(sub);
+		} else if (dev->dirents[i].is_device()) {
+			pp_device_ptr sub = dev->dirents[i].device();
+			dump_device(sub);
+		}
+	}
+}
+
+int
+test_pp_device()
+{
+	int ret = 0;
+
+	/* test the basic constructor */
+	pp_device_ptr dev(new pp_device);
+
+
+	/* define a constant */
+	pp_value val1(1);
+	dev->add_constant("const1", val1);
+	pp_value val2 = dev->constants["const1"];
+	if (val2 != val1) {
+		PP_TEST_ERROR("pp_device::add_constant()");
+		ret++;
+	}
+
+	/* define a datatype */
+	pp_datatype_ptr type1(new pp_int);
+	dev->add_datatype("type1", type1); //FIXME: handle errors?
+	pp_datatype_ptr type2 = dev->datatypes["type1"];
+	if (type2 != type1) {
+		PP_TEST_ERROR("pp_device::add_datatype()");
+		ret++;
+	}
+	pp_datatype_ptr type3 = dev->datatypes[0];
+	if (type3 != type1) {
+		PP_TEST_ERROR("pp_device::add_datatype()");
+		ret++;
+	}
+
+	/* test adding a field */
+
+	pp_binding_ptr bind1(new pp_test_binding);
+	pp_register_ptr reg1(new pp_register(bind1, 1, BITS16));
+	pp_direct_field_ptr field1(new pp_direct_field(type1));
+	field1->add_regbits(reg1, 0, pp_value(0xffff), 0);
+	dev->add_field("field1", field1);
+	pp_field_ptr field2 = dev->dirents["field1"].field();
+	if (field2 != field1) {
+		PP_TEST_ERROR("pp_device::add_field()");
+		ret++;
+	}
+
+	/* test spaces */
+	pp_space_ptr space1(new pp_space(bind1));
+	space1->add_constant("one", 1);
+	space1->add_constant("two", 2);
+	space1->add_constant("three", 3);
+	dev->add_space("space1", space1);
+
+	/* test sub-devices */
+	pp_device_ptr dev2(new pp_device);
+	dev2->add_constant("one", 1);
+	dev2->add_constant("two", 2);
+	dev2->add_constant("three", 3);
+	dev->add_device("subdevice", dev2);
+
+	//dump_device(dev);
+
+	return ret;
+}
+
+int
+main()
+{
+	int r;
+
+	r = test_pp_device();
+	if (r) return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
+}
