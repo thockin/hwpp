5bc2d261a818fc0ec842513c343b76e704dded0f
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2007-11-19 05:49:00 +0000

    * Simplify the MSR driver by combining files and making a generic simple_binding template.

--- drivers/msr/msr_binding.h
+++ drivers/msr/msr_binding.h
@@ -2,68 +2,95 @@
 #ifndef PP_DRIVERS_MSR_MSR_BINDING_H__
 #define PP_DRIVERS_MSR_MSR_BINDING_H__
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdint.h>
-#include <errno.h>
-
+#include "pp.h"
 #include "pp_binding.h"
-#include "msr_driver.h"
-#include "linux_msr_io.h"
+#include "pp_driver.h"
+#include "filesystem.h"
+
+/*
+ * msr_address
+ */
+struct msr_address
+{
+	/* constructors */
+	msr_address()
+	    : cpu(-1)
+	{
+	}
+	msr_address(int c)
+	    : cpu(c)
+	{
+	}
+
+	int cpu;
+};
+
+inline bool
+operator<(const msr_address &left, const msr_address &right)
+{
+	return (left.cpu < right.cpu);
+}
+
+inline std::ostream &
+operator<<(std::ostream& out, const msr_address &addr)
+{
+	out << "msr<" << addr.cpu << ">";
+	return out;
+}
+
+/*
+ * msr_io - Linux-specific MSR access
+ */
+
+class msr_io
+{
+    public:
+	msr_io(int cpu,
+	    const string &devdir = "", int major = -1, int minor = -1);
+	msr_io(const msr_address &address,
+	    const string &devdir = "", int major = -1, int minor = -1);
+	~msr_io();
+
+	const msr_address &
+	address() const;
+
+	pp_value
+	read(const pp_regaddr address, const pp_bitwidth width) const;
+
+	void
+	write(const pp_regaddr address, const pp_bitwidth width,
+	    const pp_value value) const;
+
+    private:
+	msr_address m_address;
+	fs::file_ptr m_file;
+
+	pp_driver_io_error
+	do_io_error(const string &str) const;
+
+	void
+	open_device(string devdir, int major, int minor);
+
+	void
+	seek(const pp_regaddr offset) const;
+
+	template<typename Tdata>
+	pp_value
+	do_read(const pp_regaddr offset) const;
+
+	template<typename Tdata>
+	void
+	do_write(const pp_regaddr offset, const pp_value value) const;
+};
 
 /*
  * msr_binding - MSR binding for register spaces
  *
  * Constructors:
- *	(msr_address address, msr_io *io?)
- *
- * Notes:
+ *	(msr_address address)
  */
-//FIXME: make a generic_binding that wraps this up
-//FIXME: use a template for the IO argument
-class msr_binding: public pp_binding
-{
-    public:
-	explicit msr_binding(msr_address address, msr_io *io = NULL)
-	    : m_address(address), m_io(io ? io : new linux_msr_io(address))
-	{
-	}
+typedef simple_binding<msr_io, msr_address> msr_binding;
 
-	virtual ~msr_binding()
-	{
-	}
-
-	virtual pp_value
-	read(const pp_regaddr address, const pp_bitwidth width) const
-	{
-		return m_io->read(address, width);
-	}
-
-	virtual void
-	write(const pp_regaddr address, const pp_bitwidth width,
-	    const pp_value value) const
-	{
-		return m_io->write(address, width, value);
-	}
-
-	virtual string
-	to_string() const
-	{
-		return ::to_string(m_address);
-	}
-
-	const msr_address &
-	address()
-	{
-		return m_address;
-	}
-
-    private:
-	msr_address m_address;
-	boost::shared_ptr<msr_io> m_io;
-};
-typedef boost::shared_ptr<msr_binding> msr_binding_ptr;
-
-#define new_msr_binding(...) msr_binding_ptr(new msr_binding(__VA_ARGS__))
+#define new_msr_binding(...) pp_binding_ptr(new msr_binding(__VA_ARGS__))
 
 #endif // PP_DRIVERS_MSR_MSR_BINDING_H__
