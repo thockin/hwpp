b5d5638da46a1c6a6f5aaad4ed7337f05abb162d
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2007-12-27 18:46:45 +0000

    Add a TEST_ASSERT primitive

--- tests/pp_test.h
+++ tests/pp_test.h
@@ -2,12 +2,27 @@
 #ifndef PP_TEST_H__
 #define PP_TEST_H__
 
+#include "pp.h"
 #include <iostream>
 
-#define PP_TEST_ERROR(msg) \
-	cerr << "ERROR: [" __FILE__ << ":" << __LINE__ << "] " << msg << endl
+#define PP_TEST_ERROR_ON_LINE(msg, file, line) \
+	std::cerr << "ERROR: [" << file << ":" << line << "] " \
+		<< msg << std::endl
+#define PP_TEST_ERROR(msg) PP_TEST_ERROR_ON_LINE(msg, __FILE__, __LINE__)
+
+#define PP_TEST_ASSERT(pred, msg) PP_TEST_ASSERT_(pred, msg, __FILE__, __LINE__)
+inline int
+PP_TEST_ASSERT_(int predicate, const string &msg, const string &file, int line)
+{
+	if (!predicate) {
+		PP_TEST_ERROR_ON_LINE(msg, file, line);
+		return 1;
+	}
+	return 0;
+}
 
 #define PP_TEST_WARNING(msg) \
-	cerr << "WARNING: [" __FILE__ << ":" << __LINE__ << "] " << msg << endl
+	std::cerr << "WARNING: [" __FILE__ << ":" << __LINE__ << "] " \
+		<< msg << std::endl
 
 #endif // PP_TEST_H__
