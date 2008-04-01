2b3983604fcf30e83bd7bdb98aa1369c9d6ea63f
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2008-04-01 05:07:15 +0000

    add comments in utils.cpp and utils.h

--- utils.h
+++ utils.h
@@ -1,10 +1,10 @@
+//
+// Copyright 2007 Google, Inc. All Rights Reserved.
+// Copyright 2008 Tim Hockin.
+//
+//FIXME: move this to fake_language.h
 #ifndef PP_UTILS_H__
 #define PP_UTILS_H__
-
-/*
- * Copyright 2007 Google Inc. All Rights Reserved.
- * Author: lesleyn@google.com (Lesley Northam)
- */
 
 #include "pp.h"
 #include "pp_path.h"
@@ -16,9 +16,11 @@
 #include "pp_binding.h"
 #include "drivers.h"
 
-/*
- * Get/set the current active context.
- */
+#include <vector>
+
+//
+// Get/set the current active context.
+//
 extern pp_context
 GET_CURRENT_CONTEXT();
 
@@ -31,7 +33,9 @@ SET_CURRENT_CONTEXT(const pp_context &new_context);
 
 #include "pp_fields.h"
 
-//FIXME: comments
+//
+// Lookup dirents by name.
+//
 extern const pp_dirent *
 GET_DIRENT(const string &path);
 extern const pp_field *
@@ -41,6 +45,9 @@ GET_REGISTER(const string &path);
 extern bool
 DEFINED(const string &path);
 
+//
+// Read/write a field, register, or regbits.
+//
 extern pp_value
 READ(const string &path);
 extern void
@@ -50,6 +57,9 @@ READ(const pp_regbits &bits);
 extern void
 WRITE(const pp_regbits &bits, const pp_value &value);
 
+//
+// Perform comparison operations on fields.
+//
 inline int
 FIELD_COMPARE(const string &field, const pp_value &comparator)
 {
@@ -132,46 +142,79 @@ FIELD_AND(const string &field, const pp_value &comparator)
 	return GET_FIELD(field)->test(comparator);
 }
 
-/*
- * NEW_PLATFORM
- * Create a new platform scope and initialize it with the built-in
- * primitive types as well as global language-defined types.
- */
+//
+// Create a new platform scope and initialize it with the built-in
+// primitive types, as well as global language-defined types.
+//
 extern pp_scope *
 NEW_PLATFORM();
 
-/*
- * OPEN_SCOPE
- * Create a new scope with the input name.  This pair of open and close
- * scope shortcut functions works like a matching bracket stack.  After
- * you open a scope all subsequent operations (from these shortcut
- * operations, eg ONE_BIT_FIELD) will be placed on this scope.
- */
+//
+// Create a new scope.  Once you open a scope, all subsequent operations
+// (such as REGN() or FIELD()) will be relative to this new scope. Unnamed
+// scopes can be named via CLOSE_SCOPE().
+//
 extern void
 OPEN_SCOPE(const string &name,
-    const pp_binding_ptr &binding = pp_binding_ptr());
-
+		const pp_binding_ptr &binding = pp_binding_ptr());
 inline void
 OPEN_SCOPE(const pp_binding_ptr &binding = pp_binding_ptr())
 {
 	OPEN_SCOPE("", binding);
 }
 
-/*
- * CLOSE_SCOPE
- * Closes the current scope context.
- */
+//
+// Close the current scope.
+//
 extern void
 CLOSE_SCOPE();
 extern void
 CLOSE_SCOPE(const string &new_name);
 
-/*
- * REGN
- * A set of shortcut functions for the creation of registers.  The created
- * register will be added onto the present scope (opened by the
- * OPEN_SCOPE function).
- */
+// These are helpers for BIND()
+struct pp_helper_driver_arg {
+	pp_helper_driver_arg(const pp_value &val): m_val(val) {}
+	pp_value m_val;
+};
+typedef std::vector<pp_value> pp_helper_driver_arg_list;
+inline pp_helper_driver_arg_list
+operator,(const pp_helper_driver_arg &lhs, const pp_value &rhs)
+{
+	pp_helper_driver_arg_list tmp;
+	tmp.push_back(lhs.m_val);
+	tmp.push_back(rhs);
+	return tmp;
+}
+inline pp_helper_driver_arg_list
+operator,(const pp_helper_driver_arg_list &lhs, const pp_value &rhs)
+{
+	pp_helper_driver_arg_list tmp(lhs);
+	tmp.push_back(rhs);
+	return tmp;
+}
+#define ARGS(...) ((pp_helper_driver_arg)__VA_ARGS__)
+//
+// Create a new binding.
+//
+// This can take an "unlimited" list of arguments, such as:
+// 	BIND("mem", ARGS(1, 2, 3, 4));
+//
+inline pp_binding_ptr
+BIND(const string &driver, const pp_helper_driver_arg_list &args)
+{
+	return pp_find_driver(driver)->new_binding(args);
+}
+inline pp_binding_ptr
+BIND(const string &driver, const pp_helper_driver_arg &arg)
+{
+	pp_helper_driver_arg_list al;
+	al.push_back(arg.m_val);
+	return BIND(driver, al);
+}
+
+//
+// Declare a named register.
+//
 extern void
 REGN(const string &name, const pp_value &address, pp_bitwidth width);
 #define REG8(name, address)   REGN(name, address, BITS8)
@@ -182,6 +225,9 @@ REGN(const string &name, const pp_value &address, pp_bitwidth width);
 
 #include "pp_regbits.h"
 
+//
+// Create a register-bits structure.
+//
 extern pp_regbits
 BITS(const string &regname);
 extern pp_regbits
@@ -192,40 +238,9 @@ BITS(const string &regname, pp_bitwidth hi_bit, pp_bitwidth lo_bit);
 #include "pp_datatype.h"
 #include "pp_datatypes.h"
 
-/*
- * Create a field and add it to the current scope.
- *
- * It can take an "unlimited" number of regbits, in the form:
- * 	FIELD("name", "type", BITS("abc", 1, 0) + BITS("def", 7, 2))
- */
-extern void
-FIELD(const string &name, const pp_datatype *type, const pp_regbits &bits);
-extern void
-FIELD(const string &name, const string &type, const pp_regbits &bits);
-
-/*
- * Create a field with a constant value.
- */
-extern void
-FIELD(const string &name, const pp_datatype *type, const pp_value &value);
-extern void
-FIELD(const string &name, const string &type, const pp_value &value);
-
-
-/*
- * Create a field which triggers procedures on read/write.
- */
-extern void
-FIELD(const string &name, const pp_datatype *type,
-    const proc_field_accessor_ptr &access);
-extern void
-FIELD(const string &name, const string &type,
-    const proc_field_accessor_ptr &access);
-#define PROCS(procs) proc_field_accessor_ptr(new procs)
-
-/*
- * Create a register and a field that consumes it.
- */
+//
+// Create a register and a field that consumes it.
+//
 extern void
 REGFIELDN(const string &name, const pp_value &address,
 		const pp_datatype *type, pp_bitwidth width);
@@ -238,131 +253,119 @@ REGFIELDN(const string &name, const pp_value &address,
 #define REGFIELD64(name, address, type)  REGFIELDN(name, address, type, BITS64)
 #define REGFIELD128(name, address, type) REGFIELDN(name, address, type, BITS128)
 
-// These are helpers for BIND()
-struct driver_arg {
-	driver_arg(const pp_value &val): m_val(val) {}
-	pp_value m_val;
-};
-typedef std::vector<pp_value> driver_arg_list;
-inline driver_arg_list
-operator,(const driver_arg &lhs, const pp_value &rhs)
-{
-	driver_arg_list tmp;
-	tmp.push_back(lhs.m_val);
-	tmp.push_back(rhs);
-	return tmp;
-}
-inline driver_arg_list
-operator,(const driver_arg_list &lhs, const pp_value &rhs)
-{
-	driver_arg_list tmp(lhs);
-	tmp.push_back(rhs);
-	return tmp;
-}
-#define ARGS(...) ((driver_arg)__VA_ARGS__)
-/*
- * Create a new binding.
- *
- * This can take an arbitrary list of arguments, such as:
- * 	BIND("mem", ARGS(1, 2, 3, 4));
- */
-inline pp_binding_ptr
-BIND(const string &driver, const driver_arg_list &args)
-{
-	return pp_find_driver(driver)->new_binding(args);
-}
-inline pp_binding_ptr
-BIND(const string &driver, const driver_arg &arg)
-{
-	driver_arg_list al;
-	al.push_back(arg.m_val);
-	return BIND(driver, al);
-}
+//
+// Create a field which gets it's value from regbits.
+//
+// It can take an "unlimited" number of regbits, in the form:
+// 	FIELD("name", "type", BITS("abc", 1, 0) + BITS("def", 7, 2))
+//
+extern void
+FIELD(const string &name, const pp_datatype *type, const pp_regbits &bits);
+extern void
+FIELD(const string &name, const string &type, const pp_regbits &bits);
 
-/*
- * INT
- * A shortcut for creating a pp_int.
- */
+//
+// Create a field from a constant value.
+//
+extern void
+FIELD(const string &name, const pp_datatype *type, const pp_value &value);
+extern void
+FIELD(const string &name, const string &type, const pp_value &value);
+
+//
+// Create a field which triggers procedures on read/write.
+//
+extern void
+FIELD(const string &name, const pp_datatype *type,
+		const proc_field_accessor_ptr &access);
+extern void
+FIELD(const string &name, const string &type,
+		const proc_field_accessor_ptr &access);
+#define PROCS(procs) proc_field_accessor_ptr(new procs)
+
+//
+// Declare an integer datatype.
+//
 extern pp_int *
 INT(const string &name, const string &units="");
-#define ANON_INT(units) INT("", units)
+inline pp_int *
+ANON_INT(const string &units="")
+{
+	return INT("", units);
+}
 
-#include <vector>
+//
+// Declare an hex datatype.
+//
+extern pp_hex *
+HEX(const string &name, const pp_bitwidth width=BITS0, const string &units="");
+inline pp_hex *
+ANON_HEX(const pp_bitwidth width=BITS0, const string &units="")
+{
+	return HEX("", width, units);
+}
 
 // These are helpers for type safety in pp_enum and pp_bitmask.
-class kvpair
+struct pp_helper_kvpair
 {
-    public:
-	kvpair(const string &k, const pp_value &v): m_key(k), m_value(v)
-	{}
-	const string &
-	key() const
-	{
-		return m_key;
-	}
-	const pp_value &
-	value() const
-	{
-		return m_value;
-	}
-
-    private:
+	pp_helper_kvpair(const string &k, const pp_value &v)
+	    : m_key(k), m_val(v) {}
 	string m_key;
-	pp_value m_value;
+	pp_value m_val;
 };
-#define KV(k,v) kvpair(k, v)
-typedef keyed_vector<string, pp_value> kvpair_list;
-inline kvpair_list
-operator,(const kvpair &lhs, const kvpair &rhs)
+#define KV(k,v) pp_helper_kvpair(k, v)
+typedef keyed_vector<string, pp_value> pp_helper_kvpair_list;
+inline pp_helper_kvpair_list
+operator,(const pp_helper_kvpair &lhs, const pp_helper_kvpair &rhs)
 {
-	kvpair_list tmp;
-	tmp.insert(lhs.key(), lhs.value());
-	tmp.insert(rhs.key(), rhs.value());
+	pp_helper_kvpair_list tmp;
+	tmp.insert(lhs.m_key, lhs.m_val);
+	tmp.insert(rhs.m_key, rhs.m_val);
 	return tmp;
 }
-inline kvpair_list
-operator,(const kvpair_list &lhs, const kvpair &rhs)
+inline pp_helper_kvpair_list
+operator,(const pp_helper_kvpair_list &lhs, const pp_helper_kvpair &rhs)
 {
-	kvpair_list tmp(lhs);
-	tmp.insert(rhs.key(), rhs.value());
+	pp_helper_kvpair_list tmp(lhs);
+	tmp.insert(rhs.m_key, rhs.m_val);
 	return tmp;
 }
 
-/*
- * Create a pp_bitmask.
- *
- * It can take an "unlimited" number of arguments, in the form:
- * 	BITMASK("name", (KV("abc", 1), KV("def", 2)))
- *
- * NOTE: In calling this, the extra parens around the KV list are required.
- * Why?  Well, C++ won't call operator,() on function arguments, so we force
- * the arguments to be a single argument, which is an expression.
- * Fortunately, a touch of macro magic makes it look like an unlimited
- * argument list.  I wish I was less proud of this.
- */
+//
+// Declare a bitmask datatype.
+//
+// It can take an "unlimited" number of arguments, in the form:
+// 	BITMASK("name", (KV("abc", 1), KV("def", 2)))
+//
+// NOTE: In calling this, the extra parens around the KV list are required.
+// Why?  Well, C++ won't call operator,() on function arguments, so we force
+// the arguments to be a single argument, which is an expression.
+// Fortunately, a touch of macro magic makes it look like an unlimited
+// argument list.  I wish I was less proud of this.
+//
 extern pp_bitmask *
-BITMASK_(const string &name, const string &dflt, const kvpair_list &kvlist);
+BITMASK_(const string &name, const string &dflt,
+		const pp_helper_kvpair_list &kvlist);
 #define BITMASK_DFLT(name, dflt, ...)	BITMASK_(name, dflt, (__VA_ARGS__))
 #define BITMASK(name, ...)		BITMASK_(name, "", (__VA_ARGS__))
 #define ANON_BITMASK(...)		BITMASK_("", "", (__VA_ARGS__))
 
-/*
- * Create a pp_enum.
- *
- * It can take an "unlimited" number of arguments, in the form:
- * 	ENUM("name", (KV("abc", 1), KV("def", 2)))
- *
- * See the NOTE for BITMASK, above.
- */
+//
+// Declare an enum datatype.
+//
+// It can take an "unlimited" number of arguments, in the form:
+// 	ENUM("name", (KV("abc", 1), KV("def", 2)))
+//
+// See the NOTE for BITMASK, above.
+//
 extern pp_enum *
-ENUM_(const string &name, const kvpair_list &kvlist);
+ENUM_(const string &name, const pp_helper_kvpair_list &kvlist);
 #define ENUM(name, ...)			ENUM_(name, (__VA_ARGS__))
 #define ANON_ENUM(...)			ENUM_("", (__VA_ARGS__))
 
-/*
- * BOOL
- * Shortcut to create a boolean.
- */
+//
+// Declare a boolean datatype.
+//
 extern pp_bool *
 BOOL(const string &name, const string &true_str, const string &false_str);
 #define ANON_BOOL(true_str, false_str) BOOL("", true_str, false_str)
