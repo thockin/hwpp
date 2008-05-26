142e8b54baf479ab4b56d43a6a870b07126144dd
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2008-05-26 16:16:21 +0000

    Don't return a string.c_str() when the string goes out of scope. Drop __func__ from parse_location. Standardize printing of parse_locations.

--- language.h
+++ language.h
@@ -4,21 +4,21 @@
 
 #include "pp.h"
 #include <exception>
+#include <ostream>
 
 class parse_location
 {
     private:
 	const char *m_file;
 	int m_line;
-	const char *m_function;
 
     public:
 	parse_location()
-	    : m_file(NULL), m_line(-1), m_function(NULL)
+	    : m_file(NULL), m_line(-1)
 	{
 	}
-	parse_location(const char *file, int line, const char *func)
-	    : m_file(file), m_line(line), m_function(func)
+	parse_location(const char *file, int line)
+	    : m_file(file), m_line(line)
 	{
 	}
 	const char *
@@ -31,13 +31,14 @@ class parse_location
 	{
 		return m_line;
 	}
-	const char *
-	function() const
-	{
-		return m_function;
-	}
 };
-#define THIS_LOCATION	parse_location(__FILE__, __LINE__, __func__)
+// Stream out a parse_location
+inline std::ostream &
+operator<<(std::ostream& o, const parse_location &loc)
+{
+	return o << loc.file() << ":" << loc.line();
+}
+#define THIS_LOCATION	parse_location(__FILE__, __LINE__)
 
 class pp_parse_error: public std::exception
 {
@@ -45,10 +46,17 @@ class pp_parse_error: public std::exception
 	string m_error;
 	parse_location m_location;
 	bool m_location_valid;
+	mutable string m_what;
 
     public:
+	// NOTE: taking a string here is a calculated risk.  It could
+	// throw during construction, assignment, or what().
 	explicit pp_parse_error(const string &error)
 	    : m_error(error), m_location_valid(false)
+	{
+	}
+	explicit pp_parse_error(const string &error, const parse_location &loc)
+	    : m_error(error), m_location(loc), m_location_valid(true)
 	{
 	}
 	~pp_parse_error() throw()
@@ -65,17 +73,14 @@ class pp_parse_error: public std::exception
 	virtual const char *
 	what() const throw()
 	{
-		string full;
+		// Yes, using a string could blow up, but I really don't
+		// want to deal with resource management or fixed buffers.
+		m_what = "";
 		if (m_location_valid) {
-			full += to_string(m_location.file())
-			     + ":"
-			     + to_string(m_location.line())
-			     + ": "
-			     + to_string(m_location.function())
-			     + ": ";
+			m_what += to_string(m_location) + ": ";
 		}
-		full += m_error;
-		return full.c_str();
+		m_what += m_error;
+		return m_what.c_str();
 	}
 };
 
