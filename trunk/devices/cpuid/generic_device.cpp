7c8431898e8a980f0e3b2658ba05bf52d7b9912e
schran <schran@6a57942a-7e51-f6c1-ad28-5202626a2061>
2008-07-23 17:15:49 +0000

    Added EAX-EDX functions for easy access to registers in CPUID encodings

--- trunk/devices/cpuid/generic_device.cpp
+++ trunk/devices/cpuid/generic_device.cpp
@@ -2,6 +2,48 @@
 #include "generic_device.h"
 #include "fake_language.h"
 
+// Functions for easy access to individual registers returned by cpuid
+inline pp_regbits
+EAX(const string &name, const pp_bitwidth &hi, const pp_bitwidth &lo)
+{
+	return BITS(name, hi, lo);
+}
+inline pp_regbits
+EAX(const string &name, const pp_bitwidth &bit)
+{
+	return BITS(name, bit);
+}
+inline pp_regbits
+EBX(const string &name, const pp_bitwidth &hi, const pp_bitwidth &lo)
+{
+	return BITS(name, hi + 32, lo + 32);
+}
+inline pp_regbits
+EBX(const string &name, const pp_bitwidth &bit)
+{
+	return BITS(name, bit + 32);
+}
+inline pp_regbits
+ECX(const string &name, const pp_bitwidth &hi, const pp_bitwidth &lo)
+{
+	return BITS(name, hi + 64, lo + 64);
+}
+inline pp_regbits
+ECX(const string &name, const pp_bitwidth &bit)
+{
+	return BITS(name, bit + 64);
+}
+inline pp_regbits
+EDX(const string &name, const pp_bitwidth &hi, const pp_bitwidth &lo)
+{
+	return BITS(name, hi + 96, lo + 96);
+}
+inline pp_regbits
+EDX(const string &name, const pp_bitwidth &bit)
+{
+	return BITS(name, bit + 96);
+}
+
 void
 CPUID_SCOPE(const string &name, const pp_value &cpu)
 {
@@ -32,18 +74,18 @@ class cpuid_family_procs: public pp_rwprocs
 	read() const
 	{
 		if (FIELD_EQ("vendor", "intel")) {
-			pp_value family = READ(BITS("%fn1", 11, 8))
-			                + READ(BITS("%fn1", 27, 20));
+			pp_value family = READ(EAX("%fn1", 11, 8))
+			                + READ(EAX("%fn1", 27, 20));
 			return family;
 		} else if (FIELD_EQ("vendor", "amd")) {
-			pp_value base_family = READ(BITS("%fn1", 11, 8));
+			pp_value base_family = READ(EAX("%fn1", 11, 8));
 			if (base_family == 0xf) {
-				return READ(BITS("%fn1", 27, 20)) + 0xf;
+				return READ(EAX("%fn1", 27, 20)) + 0xf;
 			}
 			return base_family;
 		}
 		WARN("unknown CPUID vendor, using base family");
-		return READ(BITS("%fn1", 11, 8));
+		return READ(EAX("%fn1", 11, 8));
 	}
 	void
 	write(const pp_value &value) const
@@ -57,16 +99,17 @@ class cpuid_model_procs: public pp_rwprocs
 	read() const
 	{
 		if (FIELD_EQ("vendor", "intel")) {
-			return READ(BITS("%fn1", 19, 16) + BITS("%fn1", 7, 4));
+			return READ(EAX("%fn1", 19, 16) + EAX("%fn1", 7, 4));
 		} else if (FIELD_EQ("vendor", "amd")) {
-			pp_value base_family = READ(BITS("%fn1", 11, 8));
+			pp_value base_family = READ(EAX("%fn1", 11, 8));
 			if (base_family == 0xf) {
-				return READ(BITS("%fn1", 19, 16) + BITS("%fn1", 7, 4));
+				return READ(EAX("%fn1", 19, 16)
+					    + EAX("%fn1", 7, 4));
 			}
-			return READ(BITS("%fn1", 7, 4));
+			return READ(EAX("%fn1", 7, 4));
 		}
 		WARN("unknown CPUID vendor, using base_model");
-		return READ(BITS("%fn1", 7, 4));
+		return READ(EAX("%fn1", 7, 4));
 	}
 	void
 	write(const pp_value &value) const
@@ -78,39 +121,41 @@ void
 cpuid_generic_device()
 {
 	REG128("%fn0", 0);
-	FIELD("largest_std_fn", "hex_t", BITS("%fn0", 31, 0));
+	FIELD("largest_std_fn", "hex_t", EAX("%fn0", 31, 0));
 	FIELD("vendor", "cpuid_vendor_t",
-			BITS("%fn0", 95, 64) +
-			BITS("%fn0", 127, 96) +
-			BITS("%fn0", 63, 32));
+			ECX("%fn0", 31, 0) +
+			EDX("%fn0", 31, 0) +
+			EBX("%fn0", 31, 0));
 
 	if (FIELD_GE("largest_std_fn", 1)) {
 		REG128("%fn1", 1);
 		FIELD("family", "int_t", PROCS(cpuid_family_procs));
 		FIELD("model", "int_t", PROCS(cpuid_model_procs));
-		FIELD("stepping", "int_t", BITS("%fn1", 3, 0));
+		FIELD("stepping", "int_t", EAX("%fn1", 3, 0));
 
 		if (FIELD_EQ("vendor", "intel")) {
 			FIELD("type", ANON_ENUM(
 					KV("oem", 0),
 					KV("overdrive", 1),
 					KV("dual", 2)),
-				BITS("%fn1", 13, 12));
+				EAX("%fn1", 13, 12));
 		}
 
 		if (FIELD_EQ("vendor", "amd")) {
 			FIELD("std_features", "cpuid_features_t",
-					BITS("%fn1", 95, 64) + BITS("%fn1", 127, 96));
+					ECX("%fn1", 31, 0)
+					+ EDX("%fn1", 31, 0));
 
-			FIELD("brand_id_8bit", "int_t", BITS("%fn1", 39, 32));
-			FIELD("clflush_size", ANON_INT("QWORDs"), BITS("%fn1", 47, 40));
+			FIELD("brand_id_8bit", "int_t", EBX("%fn1", 7, 0));
+			FIELD("clflush_size", ANON_INT("QWORDs"),
+					EBX("%fn1", 15, 8));
 			if (FIELD_TEST("std_features", "htt")) {
 				FIELD("logical_proc_count", "int_t",
-						BITS("%fn1", 55, 48));
+						EBX("%fn1", 23, 16));
 			} else {
 				FIELD("logical_proc_count", "int_t", 1);
 			}
-			FIELD("init_lapic_id", "int_t", BITS("%fn1", 63, 56));
+			FIELD("init_lapic_id", "int_t", EBX("%fn1", 31, 24));
 		}
 	}
 
