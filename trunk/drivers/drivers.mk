6fcdc0e67b1bfb1fcd7596c5a0d1dc3e14c2689d
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2008-10-10 18:26:16 +0000

    Fix depclean for drivers

--- trunk/drivers/drivers.mk
+++ trunk/drivers/drivers.mk
@@ -1,4 +1,4 @@
-TOPDIR ?= ../..
+TOPDIR ?= $(shell pwd)/../..
 include $(TOPDIR)/pp.mk
 
 DRIVER_TARGET = $(DRIVER_NAME)_driver_module.o
@@ -20,5 +20,9 @@ test: all
 clean:
 	@$(RM) $(OBJS) $(DRIVER_TARGET) *.o
 	@$(MAKE) -C tests clean
+
+.PHONY: depclean
+depclean:
+	@$(MAKE) -C tests depclean
 
 .depend: $(SRCS)
