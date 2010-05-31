1709dff6d2af9b0a956c53e2e26d9e4b0d7bb20d
thockin <thockin@6a57942a-7e51-f6c1-ad28-5202626a2061>
2010-05-31 06:45:45 +0000

    Add NeverNullScopedPtr and MaybeNullScopedPtr

---
+++ trunk/util/pointer.h
@@ -0,0 +1,109 @@
+/* Copyright (c) Tim Hockin, 2010 */
+#ifndef PP_UTIL_POINTER_H__
+#define PP_UTIL_POINTER_H__
+
+#include <boost/scoped_ptr.hpp>
+#include "pp/util/assert.h"
+
+namespace util {
+
+struct NullPointerError: public std::runtime_error
+{
+	explicit NullPointerError(const std::string &str)
+	    : runtime_error(str)
+	{
+	}
+};
+
+// This is a scoped_ptr<> that can never be NULL.
+template<typename Tptr>
+class NeverNullScopedPtr : public boost::scoped_ptr<Tptr>
+{
+ private:
+	// This allows us to strip the const off of Tptr, if present.  This
+	// allows us to have const and non-const ctors.
+	template<typename T>
+	struct UnConst {
+		typedef T type;
+	};
+	template<typename T>
+	struct UnConst<const T> {
+		typedef T type;
+	};
+
+ public:
+	NeverNullScopedPtr(typename UnConst<Tptr>::type *pointer)
+	    : boost::scoped_ptr<Tptr>(pointer)
+	{
+		if (pointer == NULL) {
+			throw NullPointerError(
+			    "NeverNullScopedPtr initialized to NULL");
+		}
+	}
+	NeverNullScopedPtr(const typename UnConst<Tptr>::type *pointer)
+	    : boost::scoped_ptr<Tptr>(pointer)
+	{
+		if (pointer == NULL) {
+			throw NullPointerError(
+			    "NeverNullScopedPtr initialized to NULL");
+		}
+	}
+	// This catches static initializations to NULL.
+	template<typename T>
+	NeverNullScopedPtr(T catches_null)
+	{
+		// This symbol does not exist, but makes for better errors.
+		dont_init_NeverNullScopedPtr_to_NULL(catches_null);
+	}
+
+	void
+	reset(typename UnConst<Tptr>::type *pointer)
+	{
+		if (pointer == NULL) {
+			throw NullPointerError(
+			    "NeverNullScopedPtr reset to NULL");
+		}
+		boost::scoped_ptr<Tptr>::reset(pointer);
+	}
+	void
+	reset(const typename UnConst<Tptr>::type *pointer)
+	{
+		if (pointer == NULL) {
+			throw NullPointerError(
+			    "NeverNullScopedPtr reset to NULL");
+		}
+		boost::scoped_ptr<Tptr>::reset(pointer);
+	}
+	// This catches static resets to NULL.
+	template<typename T>
+	void reset(T catches_null)
+	{
+		// This symbol does not exist, but makes for better errors.
+		dont_reset_NeverNullScopedPtr_to_NULL(catches_null);
+	}
+
+	void
+	swap(NeverNullScopedPtr &other)
+	{
+		boost::scoped_ptr<Tptr>::swap(other);
+	}
+
+	// This prevents swap() with NULL.  It's never defined.
+	void
+	swap(boost::scoped_ptr<Tptr> &other);
+};
+
+// This is a scoped_ptr<> that can be NULL.
+template<typename Tptr>
+class MaybeNullScopedPtr : public boost::scoped_ptr<Tptr>
+{
+ public:
+	MaybeNullScopedPtr(Tptr *pointer = NULL)
+	    : boost::scoped_ptr<Tptr>(pointer)
+	{
+	}
+};
+
+}  // namespace util
+
+#endif // PP_UTIL_POINTER_H__
---
+++ trunk/util/tests/pointer_test.cpp
@@ -0,0 +1,62 @@
+#include "pp/pp.h"
+#include "pp/util/pointer.h"
+#include "pp/util/test.h"
+
+namespace util {
+
+TEST(test_never_null_scoped_ptr)
+{
+	{
+		//NeverNullScopedPtr<int> nnsp(NULL);  // should fail to compile
+	}
+	{
+		int *p = NULL;
+		try {
+			NeverNullScopedPtr<int> nnsp(p);
+		} catch (NullPointerError &e) {
+			// Expected.
+		} catch (...) {
+			TEST_FAIL("NeverNullScopedPtr::NeverNullScopedPtr()");
+		}
+	}
+	{
+		NeverNullScopedPtr<int> nnsp(new int(93));
+		TEST_ASSERT(*nnsp == 93);
+		*nnsp = 76;
+		TEST_ASSERT(*nnsp == 76);
+		nnsp.reset(new int(42));
+		TEST_ASSERT(*nnsp == 42);
+		int *p = NULL;
+		try {
+			nnsp.reset(p);
+		} catch (NullPointerError &e) {
+			// Expected.
+		} catch (...) {
+			TEST_FAIL("NeverNullScopedPtr::reset()");
+		}
+	}
+	{
+		const int *p1 = new int(93);
+		NeverNullScopedPtr<const int> nnsp(p1);
+		TEST_ASSERT(*nnsp == 93);
+		int *p2 = new int(76);
+		nnsp.reset(p2);
+		TEST_ASSERT(*nnsp == 76);
+		const int *p3 = new int(42);
+		nnsp.reset(p3);
+		TEST_ASSERT(*nnsp == 42);
+	}
+	{
+		NeverNullScopedPtr<int> nnsp1(new int(93));
+		NeverNullScopedPtr<int> nnsp2(new int(76));
+		TEST_ASSERT(*nnsp1 == 93);
+		TEST_ASSERT(*nnsp2 == 76);
+		nnsp1.swap(nnsp2);
+		TEST_ASSERT(*nnsp1 == 76);
+		TEST_ASSERT(*nnsp2 == 93);
+		boost::scoped_ptr<int> sp(new int(42));
+		//nnsp1.swap(sp);  // should fail to compile
+	}
+}
+
+} // namespace util
